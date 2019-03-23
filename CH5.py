71f3305435747ecf124620d27d550df85175c378
GTVERZOSA <gtverzosa@gmail.com>
2019-03-22 22:57:11 -0700

    Chapter 5

---
+++ CH5.py
@@ -0,0 +1,92 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age = 13
+>>> if age > 20:
+	print('You are too old!')
+
+	
+>>> age = 25
+>>> if age > 20:
+	print('You are too old!')
+	print('Why are you here?')
+	print('Why aren\'t you mowing the lawn or sorting papers?')
+
+	
+You are too old!
+Why are you here?
+Why aren't you mowing the lawn or sorting papers?
+>>> age = 25
+>>> if age > 20:
+	print('You are too old!')
+	print('Why are you here?')
+	print('Why aren\'t you mowing the lawn or sorting papers?')
+
+	
+You are too old!
+Why are you here?
+Why aren't you mowing the lawn or sorting papers?
+>>> if age > 20:
+	print('You are too old!')
+	  print('Why are you here?')
+	  
+SyntaxError: unexpected indent
+>>> age = 10
+>>> if age > 10:
+	print('You are too old for my jokes!')
+
+	
+>>> age = 10
+>>> if age >= 10:
+	print('You are too old for my jokes!')
+
+	
+You are too old for my jokes!
+>>> age = 10
+>>> if age 10 == 10:
+	
+SyntaxError: invalid syntax
+>>> if age == 10:
+	print('What\'s brown and sticky? A strick!!')
+
+	
+What's brown and sticky? A strick!!
+>>> print("Want to hear a dirty joke?")
+Want to hear a dirty joke?
+>>> age = 12
+>>> if age == 12:
+	print("A pig fell in the mud!")
+else:
+	print("Shh. It's a secret.")
+
+	
+A pig fell in the mud!
+>>> print("Want to hear a dirty joke?")
+Want to hear a dirty joke?
+>>> age = 8
+>>> if age == 12:
+	print("A pig fell in the mud!")
+else:
+	print("Shh. It's a secret.")
+
+	
+Shh. It's a secret.
+>>> age = 12
+>>> if age == 10:
+	print("What do you call an unhappy cranberry?")
+	print("A blueberry!")
+elif age == 11:
+	print("What did the green grape say to the blue grape?")
+	print("Breathe! Breathe!")
+elif age == 12:
+	print("What did 0 say to 8?")
+	print("Hi guys!")
+elif age == 13:
+	print("Why wasn't 10 afraid of 7?")
+	print("Because rather than eating 9, 8 7 pi.")
+else:
+	print("Huh?")
+
+	
+What did 0 say to 8?
+Hi guys!
+>>> 
