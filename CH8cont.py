efbbe0b5d59d3b1af3a38544c8219a98961507fd
GTVERZOSA <gtverzosa@gmail.com>
2019-03-22 23:02:18 -0700

    Chapter 8 Cont.

---
+++ CH8cont.py
@@ -0,0 +1,14 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> avery = turtle.Pen()
+>>> kate = turtle.Pen()
+>>> avery.forward(50)
+>>> avery.right(90)
+>>> avery.forward(20)
+>>> kate.left(90)
+>>> kate.forward(100)
+>>> jacob = turtle.Pen()
+>>> jacob.left(180)
+>>> jacob.forward(80)
+>>> 
