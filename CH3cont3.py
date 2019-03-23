da6cc6315e321f7732b6c39d54c1463344d941e8
GTVERZOSA <gtverzosa@gmail.com>
2019-03-22 22:55:29 -0700

    Chapter 3 Cont. 3

---
+++ CH3cont3.py
@@ -0,0 +1,15 @@
+favorite_sports = ['Ralph Williams, Football', 'Michael Tippet, Basketball', 'Edward Elgar, Baseball', 'Rebecca Clarke, Netball', 'Ethel Smyth, Badminton', 'Frank Bridge, Rugby']
+favorite_sports = {'Ralph Williams' : 'Football', 'Michael Tippet' : 'Basketball', 'Edward Elgar' : 'Baseball', 'Rebecca Clarke' : 'Netball', 'Ethel Smyth' : 'Badminton', 'Frank Bridge' : 'Rugby'}
+print(favorite_sports['Rebecca Clarke'])
+Netball
+del favorite_sports['Ethel Smyth']
+print(favorite_sports)
+{'Ralph Williams': 'Football', 'Michael Tippet': 'Basketball', 'Edward Elgar': 'Baseball', 'Rebecca Clarke': 'Netball', 'Frank Bridge': 'Rugby'}
+favorite_sports['Ralph Williams'] = 'Ice Hockey'
+print(favorite_sports)
+{'Ralph Williams': 'Ice Hockey', 'Michael Tippet': 'Basketball', 'Edward Elgar': 'Baseball', 'Rebecca Clarke': 'Netball', 'Frank Bridge': 'Rugby'}
+favorite_sports = {'Rebecca Clarke' : 'Netball', 'Michael Tippet' : 'Basketball', 'Ralph Williams' : 'Ice Hockey', 'Edward Elgar' : 'Baseball', 'Frank Bridge' : 'Rugby'}
+favorite_colors = {'Maclom Warner' : 'Pink polka dots', 'James Baxter' : 'Orange stripes', 'Sue Lee' : 'Purple paisley'}
+favorite_sports + favorite_colors
+
+
