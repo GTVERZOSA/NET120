32be50c6b5769a2c87a9136122def0e081980401
GTVERZOSA <gtverzosa@gmail.com>
2019-03-22 22:52:55 -0700

    Chapter 3 Cont.

---
+++ CH3cont.py
@@ -0,0 +1,42 @@
+wizard_list = 'spider legs, toe of frog, eye of newt, bat wing, slug butter, snake dandruff'
+print(wizard_list)
+spider legs, toe of frog, eye of newt, bat wing, slug butter, snake dandruff
+wizard_list = ['spider legs', 'toe of frog', 'eye of newt', 'bat wing', 'slug butter', 'snake dandruff']
+print(wizard_list)
+['spider legs', 'toe of frog', 'eye of newt', 'bat wing', 'slug butter', 'snake dandruff']
+print(wizard_list[2])
+eye of newt
+wizard_list[2] = 'snail tongue'
+print(wizard_list)
+['spider legs', 'toe of frog', 'snail tongue', 'bat wing', 'slug butter', 'snake dandruff']
+print(wizard_list[2:5])
+['snail tongue', 'bat wing', 'slug butter']
+some_numbers = [1,2,5,10,20]
+some_strings = ['Which', 'Witch', 'Is', 'Which']
+numbers_and_strings = ['Why', 'was', 'afraid', 'of', 7,'because',7,8,9]
+print(numbers_and_strings)
+['Why', 'was', 'afraid', 'of', 7, 'because', 7, 8, 9]
+numbers_and_strings = ['Why', 'was',6, 'afraid', 'of', 7,'because',7,8,9]
+print(numbers_and_strings)
+['Why', 'was', 6, 'afraid', 'of', 7, 'because', 7, 8, 9]
+print(some_strings)
+['Which', 'Witch', 'Is', 'Which']
+print(some_numbers)
+[1, 2, 5, 10, 20] 
+numbers = [1,2,3,4]
+strings = ['I','kicked', 'my','toe','and','it','is','sore']
+mylist = [numbers, strings]
+print(mylist)
+[[1, 2, 3, 4], ['I', 'kicked', 'my', 'toe', 'and', 'it', 'is', 'sore']]
+wizard_list.append('bear burp')
+print(wizard_list)
+['spider legs', 'toe of frog', 'snail tongue', 'bat wing', 'slug butter', 'snake dandruff', 'bear burp']
+wizard_list.append('mandrake')
+wizard_list.append('hemlock')
+wizard_list.append('swamp gas')
+print(wizard_list)
+['spider legs', 'toe of frog', 'snail tongue', 'bat wing', 'slug butter', 'snake dandruff', 'bear burp', 'mandrake', 'hemlock', 'swamp gas']
+del wizard_list [5]
+print(wizard_list)
+['spider legs', 'toe of frog', 'snail tongue', 'bat wing', 'slug butter', 'bear burp', 'mandrake', 'hemlock', 'swamp gas']
+ 
